3b8f939d26979e19ab1eb3f56ee404caf48a8755
Gambondi <108482548+Gambondi@users.noreply.github.com>
2024-08-09 18:50:51 -0500

    fix: layout

--- inventory_management/app/Utils/firebase.js
+++ inventory_management/app/Utils/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
-import {getFirestore} from "firebase/firestore";
-var firebase = require('./firebase');
-var firebaseui = require('firebaseui');
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
 const firestore = getFirestore(app);
-
-export {firestore, firebase, firebaseui }
+const provider = new GoogleAuthProvider();
+export { app, provider, firestore };
--- inventory_management/app/components/page.js
+++ inventory_management/app/components/page.js
@@ -24,7 +24,7 @@ import {
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-import { firestore } from '@/firebase';
+import { firestore } from '@/app/firebase';
 import {
   collection,
   doc,
